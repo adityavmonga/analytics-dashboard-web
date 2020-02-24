fb2d03344d7ded656fbea2784d521196f2848f5f
Aditya Vikram Monga <aditya@intelluslearning.com>
2020-02-24 12:27:50 +0530

    first routerlink and material installation

--- dashboard-web/src/app/app-routing.module.ts
+++ dashboard-web/src/app/app-routing.module.ts
@@ -5,8 +5,10 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   // { path: '', redirectTo: 'dashboard', pathMatch:'full' },
   { path: '', redirectTo: 'app', pathMatch:'full' },
-  { path: 'app', loadChildren: './modules/landing/landing.module#LandingModule'},
-  { path: 'dashboard', loadChildren: './modules/dashboard/dashboard.module#DashboardModule'},
+  // { path: 'app', loadChildren: './modules/landing/landing.module#LandingModule'},
+  // { path: 'dashboard', loadChildren: './modules/dashboard/dashboard.module#DashboardModule'},
+  { path: 'app', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule)},
+  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)},
 ];
 
 @NgModule({
