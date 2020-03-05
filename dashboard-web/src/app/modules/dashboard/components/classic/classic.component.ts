e2701f582213d65587b3101245f0b285d29fae55
Aditya Vikram Monga <aditya@intelluslearning.com>
2020-03-05 17:13:28 +0530

    working prototype

--- dashboard-web/src/app/modules/dashboard/components/classic/classic.component.ts
+++ dashboard-web/src/app/modules/dashboard/components/classic/classic.component.ts
@@ -22,11 +22,14 @@ export class ClassicComponent implements OnInit{
   type: string = 'Column2d';
   activityChartOptions: any;
   userChartOptions: any;
+  instChartOptions: any;
+  demoChartOptions: any;
+
   cards = [
-    { title: 'Activity', chartOptions: this.activityChartOptions},
-    { title: 'Users', chartOptions: this.userChartOptions},
-    { title: 'Institutes', chartOptions: this.activityChartOptions},
-    { title: 'Demographics', chartOptions: this.activityChartOptions}
+    { title: 'Activity', chartOptions: this.activityChartOptions, isDataAvailable: false},
+    { title: 'Users', chartOptions: this.userChartOptions, isDataAvailable: false},
+    { title: 'Institutes', chartOptions: this.instChartOptions, isDataAvailable: false},
+    { title: 'Demographics', chartOptions: this.demoChartOptions, isDataAvailable: false}
   ]
   
   constructor(private apiService: DashboardApiService) {
@@ -35,17 +38,20 @@ export class ClassicComponent implements OnInit{
   ngOnInit() { }
   
   ngOnChanges(){
-    this.plotActivity()
-    this.isDataAvailable = true;
+    this.plotActivity();
+    this.plotUsers();
+    this.plotInst();
+    this.plotDemographics();
+    // this.isDataAvailable = true;
   };
   
   plotActivity() {
     this.apiService.getActivity(this.eventName).subscribe((resp) => {
-    this.activityChartOptions = {   
-      chart: {
-        zoomType: 'x'
-      },
-      title: {
+      this.activityChartOptions = {   
+        chart: {
+          zoomType: 'x'
+        },
+        title: {
         text: 'User Activity'
       },
       subtitle: {
@@ -58,45 +64,48 @@ export class ClassicComponent implements OnInit{
         minRange: 7 * 24 * 3600000 // seven days
       },
       yAxis: {          
-         title:{
+        title:{
           text: 'Beacons Fired'
-         } 
+        } 
       },
       legand:{
         enabled: false
       },
       // tooltip: {
-      //    valueSuffix:" °C"
-      // },
-      plotOptions : {
-         area: {
+        //    valueSuffix:" °C"
+        // },
+        plotOptions : {
+          area: {
             fillColor: {
-               linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
-               stops: [
+              linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+              stops: [
                   [0, Highcharts.getOptions().colors[0]],
                   [1, new Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-               ]
+                ]
             },
             marker: {
-               radius: 2
+              radius: 2
             },
             lineWidth: 1,
             states: {
-               hover: {
-                  lineWidth: 1
-               }
+              hover: {
+                lineWidth: 1
+              }
             },
             threshold: null
-         }
-      },
-      series: [{
-        type: 'area',
-        name: 'user activity',
-        pointInterval: 24 * 3600 * 1000,
-        pointStart: Date.UTC(2019,11,3),
-        data: resp
-      }]
-    }
+          }
+        },
+        series: [{
+          type: 'area',
+          name: 'user activity',
+          pointInterval: 24 * 3600 * 1000,
+          // pointStart: Date.UTC(2019,11,6),
+          pointStart: new Date().setDate((new Date()).getDate() - 95),
+          data: resp
+        }]
+      };
+      this.cards[0].chartOptions = this.activityChartOptions;
+      this.cards[0].isDataAvailable = true;
     });
   }
 
@@ -154,10 +163,140 @@ export class ClassicComponent implements OnInit{
           type: 'area',
           name: 'users',
           pointInterval: 24 * 3600 * 1000,
-          pointStart: Date.UTC(2019,11,3),
+          // pointStart: Date.UTC(2019,11,6),
+          pointStart: new Date().setDate((new Date()).getDate() - 95),
           data: resp
         }]
       }
+      this.cards[1].chartOptions = this.userChartOptions;
+      this.cards[1].isDataAvailable = true;
       });
+  }
+
+  plotInst(){
+    this.apiService.getInst(this.eventName).subscribe((resp) => {
+      this.instChartOptions = {   
+        chart: {
+          zoomType: 'x'
+        },
+        title: {
+          text: 'Total Institutes'
+        },
+        subtitle: {
+          text: document.ontouchstart === undefined ?
+          'Click and drag in the plot area to zoom in' :
+          'Pinch the chart to zoom in'
+        },
+        xAxis:{
+          type: 'datetime',
+          minRange: 7 * 24 * 3600000 // seven days
+        },
+        yAxis: {          
+           title:{
+            text: 'Number of distinct institutes'
+           } 
+        },
+        legand:{
+          enabled: false
+        },
+        // tooltip: {
+        //    valueSuffix:" °C"
+        // },
+        plotOptions : {
+           area: {
+              fillColor: {
+                 linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+                 stops: [
+                    [0, Highcharts.getOptions().colors[0]],
+                    [1, new Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                 ]
+              },
+              marker: {
+                 radius: 2
+              },
+              lineWidth: 1,
+              states: {
+                 hover: {
+                    lineWidth: 1
+                 }
+              },
+              threshold: null
+           }
+        },
+        series: [{
+          type: 'area',
+          name: 'inst',
+          pointInterval: 24 * 3600 * 1000,
+          // pointStart: Date.UTC(2019,11,6),
+          pointStart: new Date().setDate((new Date()).getDate() - 95),
+          data: resp
+        }]
+      }
+      this.cards[2].chartOptions = this.instChartOptions;
+      this.cards[2].isDataAvailable = true;
+    });
+  }
+
+  plotDemographics() {
+    this.demoChartOptions = {
+      chart: {
+      type: 'pie'
+    },
+    title: {
+        text: 'Usage by Country'
+    },
+    subtitle: {
+        text: 'Click the slices to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
+    },
+
+    accessibility: {
+        announceNewData: {
+            enabled: true
+        },
+        point: {
+            valueSuffix: '%'
+        }
+    },
+
+    plotOptions: {
+        series: {
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}: {point.y:.1f}%'
+            }
+        }
+    },
+
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+    },
+
+    series: [
+        {
+            name: "Countries",
+            colorByPoint: true,
+        }
+      ],
+    }
+
+    this.plotCountry();
+    // this.plotCity();
+  }
+  
+  plotCountry() {
+    this.apiService.getCountry(this.eventName).subscribe((resp) => {
+      this.demoChartOptions.series[0].data = resp;
+      this.cards[3].chartOptions = this.demoChartOptions;
+      this.cards[3].isDataAvailable = true;
+    });
+  }
+
+  plotCity() {
+    this.apiService.getCity(this.eventName).subscribe((resp) => {
+      this.demoChartOptions.drilldown = {
+        series: resp
+      }
+    });
   }
 }
--- dashboard-web/src/app/modules/dashboard/services/dashboard-api.service.ts
+++ dashboard-web/src/app/modules/dashboard/services/dashboard-api.service.ts
@@ -8,6 +8,10 @@ export class DashboardApiService {
   private apiDomain = 'http://localhost:8000';
   private activityUrl = '/daywise_activity';
   private usersUrl = '/user_data';
+  private instUrl = '/event_institution_data'
+  private countryUrl = '/country_list_data';
+  private cityUrl = '/city_list_data';
+
   constructor(
     private http: HttpClient
     ) { }
@@ -15,18 +19,41 @@ export class DashboardApiService {
     getActivity(event_name){
       console.log(event_name);
       let params = new HttpParams();
-      params = params.append('event_name', event_name.toString());
+      params = params.append('event', event_name.toString());
   
-      return this.http.get<any>(this.apiDomain  + this.activityUrl, {params: params});
+      return this.http.get<number[]>(this.apiDomain  + this.activityUrl, {params: params});
       
     }
     getUsers(event_name){
       console.log(event_name);
       let params = new HttpParams();
-      params = params.append('event_name', event_name.toString());
+      params = params.append('event', event_name.toString());
   
-      return this.http.get<any>(this.apiDomain  + this.usersUrl, {params: params});
+      return this.http.get<number[]>(this.apiDomain  + this.usersUrl, {params: params});
       
     }
+    getInst(event_name){
+      console.log(event_name);
+      let params = new HttpParams();
+      params = params.append('event', event_name.toString());
   
+      return this.http.get<number[]>(this.apiDomain  + this.instUrl, {params: params});
+      
+    }
+    getCountry(event_name){
+      console.log(event_name);
+      let params = new HttpParams();
+      params = params.append('event', event_name.toString());
+  
+      return this.http.get<any>(this.apiDomain  + this.countryUrl, {params: params});
+      
+    }
+    getCity(event_name){
+      console.log(event_name);
+      let params = new HttpParams();
+      params = params.append('event', event_name.toString());
+  
+      return this.http.get<any>(this.apiDomain  + this.cityUrl, {params: params});
+      
+    }
   }
